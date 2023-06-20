3cf276b47569ea243e10d6ab7d1712e5488ee922
Fiuva <rgm745@gmail.com>
2023-06-20 18:15:18 +0200

    aaaaaaaaaaaaaaaaa

--- functions/custom.js
+++ functions/custom.js
@@ -1,20 +1,25 @@
+const diainternacionalde = require("../src/api");
+
 module.exports.handler = async (event) => {
     const { path } = event;
 
     const split = path.split('/custom')[1].split("/")
 
-    if (split.length >= 2) {
+    if (split.length >= 3) {
         const mes = split[1];
         const dia = split[2];
+
+        const did = await diainternacionalde.getCategorizedResultsWithDate(mes, dia);
+
         // Lógica para la ruta /custom/:mes/:dia
         return {
-            body: `Custom Function - Mes: ${mes}, Día: ${dia}`,
+            body: JSON.stringify(did),
             statusCode: 200
         };
     } else {
         // Ruta no encontrada
         return {
-            body: "/.netlify/functions/custom/:mes/:dia",
+            body: "Error -> /.netlify/functions/custom/:mes/:dia",
             statusCode: 404
         };
     }
